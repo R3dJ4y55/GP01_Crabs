c330b3a4a14b732eeec61af59d19a397ac7d4798
R3dJ4y55 <2535641@edu.vaniercollege.qc.ca>
2025-09-04 13:00:59 -0400

    Uploaded final version

---
+++ modern-crab/Crab.java
@@ -0,0 +1,28 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Crab extends Actor
+{
+
+    /* (World, Actor, GreenfootImage, Greenfoot and MouseInfo)*/
+
+    /**
+     * Act - do whatever the Crab wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(4);
+        /* Higher absolute value of the turn() leads to tighter turns*/
+        if (Greenfoot.isKeyDown("left")) {
+            turn(-3);
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            turn(3);
+        }
+        /* When both left and right are pressed, the crab will move -3Deg and 3Deg simultainiously This leads to the crabs going straight*/
+    }
+}
---
+++ modern-crab/Lobster.java
@@ -0,0 +1,33 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Lobster extends Actor
+{
+
+    /**
+     * Act - do whatever the Lobster wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(4);
+        if (Greenfoot.isKeyDown("A")) {
+            turn(-3);
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            turn(3);
+        }
+        if (Greenfoot.isKeyDown("E")) {
+            if (Greenfoot.ask("Eat?").equalsIgnoreCase("yes")) {
+                Greenfoot.playSound("eating.wav");
+            }
+            else {
+                Greenfoot.ask("You Sure?");
+            }
+        }
+    }
+}
